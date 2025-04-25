378406e35af5cbc700c12ab91d65c019ade0f4a8
Bruno Tripoloni <bruno.tripoloni@gmail.com>
2025-04-25 01:12:00 +0000

    Refactor forward_args.sh to filter out USER_ID and GROUP_ID from arguments before passing to main.py

--- forward_args.sh
+++ forward_args.sh
@@ -1,5 +1,13 @@
-# This script will forward all arguments to the main.py script
+# This script will forward all arguments to the main.py script, excluding USER_ID and GROUP_ID
 #!/bin/bash
 
-# Forward all arguments to the main.py script
-/opt/conda/bin/python main.py "$@"
+# Filter out USER_ID and GROUP_ID arguments
+filtered_args=()
+for arg in "$@"; do
+  if [[ "$arg" != "USER_ID="* && "$arg" != "GROUP_ID="* ]]; then
+    filtered_args+=("$arg")
+  fi
+done
+
+# Forward the filtered arguments to the main.py script
+/opt/conda/bin/python main.py "${filtered_args[@]}"
