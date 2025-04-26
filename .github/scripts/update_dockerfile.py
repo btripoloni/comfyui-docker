8be91e92acbdffc5a8bc3da29299f781dc6b7087
Bruno Tripoloni <bruno.tripoloni@gmail.com>
2025-04-26 19:41:31 -0300

    Refactor update_dockerfile.py to improve comments and adjust regex for version tags

--- .github/scripts/update_dockerfile.py
+++ .github/scripts/update_dockerfile.py
@@ -2,7 +2,7 @@ import sys
 import re
 from packaging import version
 
-# Read versions from file
+# Read versions from the file
 with open(sys.argv[1], 'r') as f:
     lines = f.readlines()
 comfyui_version = None
@@ -16,14 +16,14 @@ if not comfyui_version or not manager_version:
     print('::error::Could not find versions in input file')
     sys.exit(1)
 
-# Read Dockerfile
+# Read the Dockerfile
 dockerfile_path = sys.argv[2]
 with open(dockerfile_path, 'r') as f:
     dockerfile = f.read()
 
-# Regex to find current versions in Dockerfile (adjust as needed)
-comfyui_re = re.compile(r'COMFYUI_VERSION=([\w\.-]+)')
-manager_re = re.compile(r'COMFYUI_MANAGER_VERSION=([\w\.-]+)')
+# Adjust regex to match COMFYUI_TAG and COMFYUI_MANAGER_TAG
+comfyui_re = re.compile(r'COMFYUI_TAG=([\w\.-]+)')
+manager_re = re.compile(r'COMFYUI_MANAGER_TAG=([\w\.-]+)')
 current_comfyui = comfyui_re.search(dockerfile)
 current_manager = manager_re.search(dockerfile)
 
@@ -34,8 +34,8 @@ if current_comfyui and current_manager:
     if (version.parse(comfyui_version) != version.parse(current_comfyui_version) or
         version.parse(manager_version) != version.parse(current_manager_version)):
         # Replace versions
-        dockerfile = comfyui_re.sub(f'COMFYUI_VERSION={comfyui_version}', dockerfile)
-        dockerfile = manager_re.sub(f'COMFYUI_MANAGER_VERSION={manager_version}', dockerfile)
+        dockerfile = comfyui_re.sub(f'COMFYUI_TAG={comfyui_version}', dockerfile)
+        dockerfile = manager_re.sub(f'COMFYUI_MANAGER_TAG={manager_version}', dockerfile)
         with open(dockerfile_path, 'w') as f:
             f.write(dockerfile)
         updated = True
